472768c7f687852445388089a75ceb61b32c26f2
nncc2004 <nncc2004@gmail.com>
2026-08-18 17:20:38 -0400

    cambios urls principal

--- nfc_music_api/apps/music/views.py
+++ nfc_music_api/apps/music/views.py
@@ -1,3 +1,4 @@
+'''
 from django.shortcuts import render
 from rest_framework.generics import RetrieveAPIView
 
@@ -9,3 +10,39 @@ class SongDetailView(RetrieveAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
     lookup_field = "uuid"
+'''
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveAPIView
+
+from .models import Song
+from .serializers import SongSerializer
+
+
+class SongDetailView(RetrieveAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+    lookup_field = "uuid"
+
+    def get(self, request, *args, **kwargs):
+        try:
+            song = self.get_object()
+        except Song.DoesNotExist:
+            return Response(
+                {"error": "Song not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        if not song.firebase_url:
+            return Response(
+                {"error": "Audio file not available"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = self.get_serializer(song)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
--- nfc_music_api/nfc_music_api/urls.py
+++ nfc_music_api/nfc_music_api/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.music.urls')),
-    path('', include('apps.firebase.urls'))
+    path('firebase/', include('apps.firebase.urls')),
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
 ]
